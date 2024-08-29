671f5bd1027a3b34e25fd7ba213b1372e9897967
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-08-29 14:22:05 -0500

    Added asdf on linux

--- home/dot_config/private_fish/conf.d/asdf.fish
+++ home/dot_config/private_fish/conf.d/asdf.fish
@@ -1,3 +1,5 @@
 if [ -r "$HOMEBREW_PREFIX/opt/asdf/libexec/asdf.fish" ]
     source "$HOMEBREW_PREFIX/opt/asdf/libexec/asdf.fish"
+else if [ -r "$HOME/.local/opt/asdf/asdf.fish" ]
+    source "$HOME/.local/opt/asdf/asdf.fish"
 end
