33efb2f7ca1ab66327863e35bbb4e1483aed1838
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-09-05 13:07:44 -0500

    Updated zsh plugins

--- home/dot_config/private_zsh/plugins.zsh
+++ home/dot_config/private_zsh/plugins.zsh
@@ -2,17 +2,14 @@
     local plugin
     local plugins=(
         fast-syntax-highlighting
-        zsh-autocomplete
+        zsh-autosuggestions
         zsh-autopair
+        zsh-history-substring-search
 
         atuin
         direnv
         zoxide
     )
-    # local plugins=(
-    #     "/opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
-    #     "/opt/homebrew/share/zsh-history-substring-search/zsh-history-substring-search.zsh"
-    # )
     for plugin in $plugins; do
         source $ZDOTDIR/plugins/$plugin/*.plugin.zsh
     done
