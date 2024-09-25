da9175471fcbefd7ca0d3ad187d1c63026fbf87e
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-09-25 14:07:50 -0500

    Added VSCode shell integration for fish

---
+++ home/dot_config/private_fish/exact_conf.d/symlink_vscode.fish
@@ -0,0 +1,1 @@
+/var/lib/flatpak/app/com.visualstudio.code/current/active/files/extra/vscode/resources/app/out/vs/workbench/contrib/terminal/browser/media/fish_xdg_data/fish/vendor_conf.d/shellIntegration.fish
