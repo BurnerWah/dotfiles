fac1ecf0a341eb0304b5882477b00611e6c69a68
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-09-26 19:31:37 -0500

    Overhauled abbreviations for fish & elvish, added my mac os tailscale alias bin, and some other stuff

--- home/dot_config/private_fish/exact_conf.d/asdf.fish
+++
@@ -1,5 +0,0 @@
-if [ -r "$HOMEBREW_PREFIX/opt/asdf/libexec/asdf.fish" ]
-    source "$HOMEBREW_PREFIX/opt/asdf/libexec/asdf.fish"
-else if [ -r "$HOME/.local/opt/asdf/asdf.fish" ]
-    source "$HOME/.local/opt/asdf/asdf.fish"
-end
