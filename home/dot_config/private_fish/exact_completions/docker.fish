e8ad225f366064d7119c2c4ba54e8635ff415fef
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-10-10 18:44:02 -0500

    Updates

--- home/dot_config/private_fish/exact_completions/docker.fish
+++ home/dot_config/private_fish/exact_completions/docker.fish
@@ -1,3 +1,6 @@
+# TODO make this fall back to docker's official completions
 if [ -f /etc/containers/nodocker -o -f /usr/lib/tmpfiles.d/podman-docker.conf ]
     complete -c docker -w podman
+else if command -qs carapace
+    carapace docker fish | source
 end
