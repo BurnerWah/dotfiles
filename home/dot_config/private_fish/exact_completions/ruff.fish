3241e8431ebf43b457749c93a149200d7b1c60a7
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-10-22 15:50:40 -0500

    Updated sentry-cli & ruff fish completions to handle mise installs

--- home/dot_config/private_fish/exact_completions/ruff.fish
+++ home/dot_config/private_fish/exact_completions/ruff.fish
@@ -1,1 +1,9 @@
-ruff generate-shell-completion fish | source
+function _ruff_shimmed
+    if ruff -V &>/dev/null
+        complete -c ruff -e
+        ruff generate-shell-completion fish | source
+        complete --do-complete=(commandline -cp)
+    end
+end
+
+complete -c ruff -f -a '(_ruff_shimmed)'
--- home/dot_config/private_fish/exact_completions/sentry-cli.fish
+++ home/dot_config/private_fish/exact_completions/sentry-cli.fish
@@ -1,3 +1,13 @@
-# Clap-based completions for sentry-cli
-# Probably not compatible with carapace unfortunately
-sentry-cli completions fish 2>/dev/null | source
+# Ensures that completions for sentry-cli don't get defined when the CLI isn't
+# actually available yet, ex. because it's a mise shim, and avoids interference
+# from the 1Password CLI.
+
+function _sentry_shimmed
+    if command sentry-cli -V &>/dev/null
+        complete -c sentry-cli -e
+        command sentry-cli completions fish 2>/dev/null | source
+        complete --do-complete=(commandline -cp)
+    end
+end
+
+complete -c sentry-cli -f -a '(_sentry_shimmed)'
