a3a82cf1fd718b408f0faa562dc00ce908153e49
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-10-25 20:14:48 -0500

    Removed WORKON_HOME and my old wrappers folder from the systemd environment, and replaced docker completion with carapace completions

--- home/dot_config/private_fish/exact_completions/docker.fish
+++
@@ -1,6 +0,0 @@
-# TODO make this fall back to docker's official completions
-if [ -f /etc/containers/nodocker -o -f /usr/lib/tmpfiles.d/podman-docker.conf ]
-    complete -c docker -w podman
-else if command -qs carapace
-    carapace docker fish | source
-end
