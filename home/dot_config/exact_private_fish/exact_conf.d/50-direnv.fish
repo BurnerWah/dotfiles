0d2f508266ddac0a70a15a99bb82dd2fdd8e93bd
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-11-05 14:10:37 -0600

    Removed direnv & fish conf.d files

--- home/dot_config/exact_private_fish/exact_conf.d/50-direnv.fish
+++
@@ -1,9 +0,0 @@
-# Note - this shouldn't be run if direnv is installed via homebrew, as it
-# already activates direnv. That having been said, the homebrew package uses
-# the cellar path instead of the opt path, so the session gets messed up when
-# direnv updates. It may be worth unlinking it and then manually activating
-# direnv anyways.
-if status is-interactive
-    and command -qs direnv
-    direnv hook fish | source
-end
