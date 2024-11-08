e6ce2a4b83f4a48f5ffdc64dd981f50929bbee66
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-11-07 19:33:41 -0600

    Started to configure neovim filetypes

--- home/dot_config/exact_nvim/lua/options.lua
+++ home/dot_config/exact_nvim/lua/options.lua
@@ -4,3 +4,11 @@ require("nvchad.options")
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+vim.filetype.add {
+  pattern = {
+    [".*%.ps1%.tmpl"] = "ps1",
+    [".*%.yaml%.tmpl"] = "yaml",
+    [".*%.zsh%.tmpl"] = "zsh",
+  },
+}
