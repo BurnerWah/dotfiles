6a26cce7375d5faa8ab48d3cf7726fecd348baf6
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-11-26 14:42:05 -0600

    Added some notes to the fish mise activation script

--- home/dot_config/exact_private_fish/exact_conf.d/30-mise-activate.fish
+++ home/dot_config/exact_private_fish/exact_conf.d/30-mise-activate.fish
@@ -1,3 +1,9 @@
+# Note - Under some conditions `mise activate --shims` can result in shims
+# being at the end of $PATH, instead of at the start. `fish_add_path -g` can
+# work around that issue, but I don't currently include any code for that in my
+# dotfiles.
+# Also, `mise doctor` will complain about shims being in the $PATH when they
+# shouldn't be. I'll fix that later.
 if command -qs mise
     if not status is-interactive
         or status is-login
