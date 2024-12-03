66a5a3b11bb21b13097238091abc0d5c0800fd13
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-12-03 13:46:46 -0600

    Made atuin fish completions use the pregenerated script provided by homebrew, if possible

--- home/dot_config/exact_private_fish/exact_completions/atuin.fish
+++ home/dot_config/exact_private_fish/exact_completions/atuin.fish
@@ -1,1 +1,6 @@
-atuin gen-completions --shell fish | source
+if set -q HOMEBREW_PREFIX
+    and [ -r "$HOMEBREW_PREFIX/share/fish/vendor_completions.d/atuin.fish" ]
+    source "$HOMEBREW_PREFIX/share/fish/vendor_completions.d/atuin.fish"
+else
+    atuin gen-completions --shell fish | source
+end
