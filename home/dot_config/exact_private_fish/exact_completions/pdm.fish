977c6c2e33353fe79089eac31a5b3bcc03410c6e
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-12-12 21:07:57 -0600

    Made pdm completions handle mise shims

--- home/dot_config/exact_private_fish/exact_completions/pdm.fish
+++ home/dot_config/exact_private_fish/exact_completions/pdm.fish
@@ -1,1 +1,10 @@
-pdm completion fish | source
+function _pdm_shimmed
+    if string match -vq "*/mise/shims/pdm" (command -s pdm)
+        or pdm -V &>/dev/null
+        complete -c pdm -e
+        pdm completion fish | source
+        complete --do-complete=(commandline -cp)
+    end
+end
+
+complete -c pdm -f -a '(_pdm_shimmed)'
