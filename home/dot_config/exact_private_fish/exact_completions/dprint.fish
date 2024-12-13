272035446e0e4fc7764eda1bfeaf0213b33e5a50
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-12-12 21:24:36 -0600

    Made dprint completion handle mise shims

--- home/dot_config/exact_private_fish/exact_completions/dprint.fish
+++ home/dot_config/exact_private_fish/exact_completions/dprint.fish
@@ -1,1 +1,10 @@
-dprint completions fish | source
+function _dprint_shimmed
+    if string match -vq "*/mise/shims/dprint" (command -s dprint)
+        or dprint -V &>/dev/null
+        complete -c dprint -e
+        dprint completions fish | source
+        complete --do-complete=(commandline -cp)
+    end
+end
+
+complete -c dprint -f -a '(_dprint_shimmed)'
