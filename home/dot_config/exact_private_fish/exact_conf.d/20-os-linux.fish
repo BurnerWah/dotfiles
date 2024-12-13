7a09ca8fb2f5b879c9aa16810d83e116e63ed19b
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-12-13 14:23:29 -0600

    Started to create a linux-specific fish config

---
+++ home/dot_config/exact_private_fish/exact_conf.d/20-os-linux.fish
@@ -0,0 +1,8 @@
+set -q XDG_CONFIG_HOME || set -gx XDG_CONFIG_HOME "$HOME/.config"
+set -q XDG_DATA_HOME || set -gx XDG_DATA_HOME "$HOME/.local/share"
+set -q XDG_CACHE_HOME || set -gx XDG_CACHE_HOME "$HOME/.cache"
+
+if set -q SSH_TTY
+    fish_add_path --path --append "$XDG_DATA_HOME/homebrew/bin"
+    fish_add_path --path --append "$XDG_DATA_HOME/homebrew/sbin"
+end
