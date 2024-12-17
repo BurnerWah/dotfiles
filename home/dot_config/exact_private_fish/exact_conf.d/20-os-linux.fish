9640bec7f436a7e2928650993d02f17ec22f7dfe
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-12-16 19:53:34 -0600

    Added ~/.local/bin to path for fish ssh instances, and set the EDITOR

--- home/dot_config/exact_private_fish/exact_conf.d/20-os-linux.fish
+++ home/dot_config/exact_private_fish/exact_conf.d/20-os-linux.fish
@@ -3,6 +3,10 @@ set -q XDG_DATA_HOME || set -gx XDG_DATA_HOME "$HOME/.local/share"
 set -q XDG_CACHE_HOME || set -gx XDG_CACHE_HOME "$HOME/.cache"
 
 if set -q SSH_TTY
+    fish_add_path --path --prepend "$HOME/.local/bin"
     fish_add_path --path --append "$XDG_DATA_HOME/homebrew/bin"
     fish_add_path --path --append "$XDG_DATA_HOME/homebrew/sbin"
+
+    # This runs before fedora's *-default-editor scripts
+    set -q EDITOR || set -gx EDITOR nvim
 end
