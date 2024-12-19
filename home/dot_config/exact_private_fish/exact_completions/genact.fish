68d48f875bb9f1c41e48c527001e1ea4bbfbd4aa
Burner <10004812+BurnerWah@users.noreply.github.com>
2024-12-19 12:53:24 -0600

    More completion stuff

--- home/dot_config/exact_private_fish/exact_completions/genact.fish
+++ home/dot_config/exact_private_fish/exact_completions/genact.fish
@@ -1,9 +1,5 @@
-# genact completion
-# genact doesn't have a manual so this is derived from the help info
-complete -fc genact
-complete -c genact -s h -l help -d "Prints help information"
-complete -c genact -s l -l list-modules -d "List available modules"
-complete -c genact -s V -l version -d "Prints version information"
-complete -xc genact -s e -l exitafter -d "Exit after running for this long"
-complete -xc genact -s m -l modules -d "Run only these modules" \
-    -a "(genact --list-modules | string match '  *' | string trim)\t"
+if set -l paths (path filter -fr $__fish_vendor_completionsdirs/genact.fish)
+    source $paths[1]
+else
+    genact --print-completions fish | source
+end
