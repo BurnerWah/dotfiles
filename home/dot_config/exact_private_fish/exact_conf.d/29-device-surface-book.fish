136a365edaebb33c351c91fe710feafc347a4c17
Burner <10004812+BurnerWah@users.noreply.github.com>
2025-01-14 19:13:15 -0600

    corrected ssh handling for surface book

--- home/dot_config/exact_private_fish/exact_conf.d/29-device-surface-book.fish
+++ home/dot_config/exact_private_fish/exact_conf.d/29-device-surface-book.fish
@@ -1,2 +1,7 @@
 fish_add_path --path --prepend "$HOME/.local/bin"
+
+if set -q SSH_TTY
+    set -q HOMEBREW_PREFIX || set -gx HOMEBREW_PREFIX "/home/linuxbrew/.linuxbrew"
+end
+
 fish_add_path --path --append "$HOMEBREW_PREFIX/bin" "$HOMEBREW_PREFIX/sbin"
