2b15d7e0f4a1616781b98f9851f5439a282cb1fa
Burner <10004812+BurnerWah@users.noreply.github.com>
2025-01-15 13:20:46 -0600

    Updated various things

--- home/readonly_Documents/readonly_PowerShell/Modules/Burner.Completions/Burner.Completions.psm1
+++ home/readonly_Documents/readonly_PowerShell/Modules/Burner.Completions/Burner.Completions.psm1
@@ -4,4 +4,3 @@
 $Script:scoop = "~\scoop\apps"
 
 . (Join-Path $Script:scoop "hyperfine\current" "autocomplete\_hyperfine.ps1")
-. (Join-Path $Script:scoop "pastel\current" "autocomplete\_pastel.ps1")
