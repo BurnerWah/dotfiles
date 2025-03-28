8546466a1d38c7b305d955cfb1aa8be87eb9517b
Burner <10004812+BurnerWah@users.noreply.github.com>
2025-03-27 22:20:57 -0500

    Updated scoopfile & winget packages

--- home/readonly_Documents/readonly_PowerShell/Modules/Burner.Completions/Burner.Completions.psm1
+++ home/readonly_Documents/readonly_PowerShell/Modules/Burner.Completions/Burner.Completions.psm1
@@ -1,6 +1,2 @@
 . (Join-Path $PSScriptRoot "Scripts" "custom.ps1")
 . (Join-Path $PSScriptRoot "Scripts" "lazy-managed.ps1")
-
-$Script:scoop = "~\scoop\apps"
-
-. (Join-Path $Script:scoop "hyperfine\current" "autocomplete\_hyperfine.ps1")
