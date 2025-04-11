476d81a459357d7e023fd697f64578ad1ca54886
Burner <10004812+BurnerWah@users.noreply.github.com>
2025-04-10 20:36:31 -0500

    Added support for kitty's shell integration over SSH & in lima VMs
    The guest needs the shell integrations installed already

--- home/dot_config/exact_private_fish/exact_conf.d/90-kitty.fish
+++ home/dot_config/exact_private_fish/exact_conf.d/90-kitty.fish
@@ -2,9 +2,24 @@
 # This is needed because numbered scripts get run before Kitty's automatic integration.
 if not set -q KITTY_FISH_XDG_DATA_DIR # This is how kitty applies shell integrations
     and status is-interactive # Require an interactive session
-    and set -q KITTY_INSTALLATION_DIR # This is intended for use with manual shell integrations
 
-    set -g KITTY_SHELL_INTEGRATION enabled
-    source "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_conf.d/kitty-shell-integration.fish"
-    set -p fish_complete_path "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_completions.d"
+    set -l shell_integration_conf "shell-integration/fish/vendor_conf.d/kitty-shell-integration.fish"
+
+    # This is intended for use with manual shell integrations
+    if set -q KITTY_INSTALLATION_DIR
+
+        set -g KITTY_SHELL_INTEGRATION enabled
+        # @fish-lsp-disable-next-line 1004
+        source "$KITTY_INSTALLATION_DIR/$shell_integration_conf"
+        set -p fish_complete_path "$KITTY_INSTALLATION_DIR/shell-integration/fish/vendor_completions.d"
+
+        # This is for SSH & Lima VMs
+    else if test "$TERM" = xterm-kitty
+        and set -q SSH_TTY
+        and set -l paths (path filter -fr "/usr/lib64/kitty/$shell_integration_conf" "/usr/lib/kitty/$shell_integration_conf")
+
+        set -g KITTY_SHELL_INTEGRATION enabled
+        source "$paths[1]"
+
+    end
 end
