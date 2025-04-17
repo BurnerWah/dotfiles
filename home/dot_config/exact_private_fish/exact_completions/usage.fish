81190308172f8b070c876665d2f8ac317eefc8cd
Burner <10004812+BurnerWah@users.noreply.github.com>
2025-04-17 13:57:25 -0500

    Fixed usage completion not sourcing, and made __fish_cache_sourced_completions be skipped in more places

--- home/dot_config/exact_private_fish/exact_completions/usage.fish
+++ home/dot_config/exact_private_fish/exact_completions/usage.fish
@@ -1,1 +1,1 @@
-__fish_cache_sourced_completions usage generate completion --usage-cmd 'usage --usage-spec' fish usage
+usage generate completion --usage-cmd 'usage --usage-spec' fish usage | source
