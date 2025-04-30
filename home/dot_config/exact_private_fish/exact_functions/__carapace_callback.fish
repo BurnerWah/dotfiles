aa4c467471a1eb1e99fe5563b36546321b21e684
Burner <10004812+BurnerWah@users.noreply.github.com>
2025-04-29 19:07:39 -0500

    made carapace callback & quote suffix more fish-native

--- home/dot_config/exact_private_fish/exact_functions/__carapace_callback.fish
+++ home/dot_config/exact_private_fish/exact_functions/__carapace_callback.fish
@@ -1,3 +1,5 @@
+# This has been modified to not use sed
 function __carapace_callback -a cmd -d 'generic carapace completion callback'
-    commandline -cp | sed "s/\$/"(__carapace_quote_suffix)"/" | sed "s/ \$/ ''/" | xargs carapace $cmd fish
+    commandline -cp | string replace -r '$' (__carapace_quote_suffix) | string replace -r ' $' " ''" | xargs carapace $cmd fish
+    # commandline -cp | sed "s/\$/"(__carapace_quote_suffix)"/" | sed "s/ \$/ ''/" | xargs carapace $cmd fish
 end
--- home/dot_config/exact_private_fish/exact_functions/__carapace_quote_suffix.fish
+++ home/dot_config/exact_private_fish/exact_functions/__carapace_quote_suffix.fish
@@ -1,8 +1,11 @@
+# This has been modified to not use sed, and to simplify thigs a little
 function __carapace_quote_suffix -d 'generic carapace quote helper'
-    if not commandline -cp | xargs echo 2>/dev/null >/dev/null
-        if commandline -cp | sed 's/$/"/' | xargs echo 2>/dev/null >/dev/null
+    if not commandline -cp | xargs echo &>/dev/null
+        if commandline -cp | string replace -r '$' '"' | xargs echo &>/dev/null
+            # sed 's/$/"/'
             echo '"'
-        else if commandline -cp | sed "s/\$/'/" | xargs echo 2>/dev/null >/dev/null
+        else if commandline -cp | string replace -r "\$" "'" | xargs echo &>/dev/null
+            # sed "s/\$/'/"
             echo "'"
         end
     else
