05700b3e05057f4b99bd6a23bb253cc1ed77741c
Burner <10004812+BurnerWah@users.noreply.github.com>
2025-11-21 21:33:56 -0600

    Improved performance of hk fish completions

--- home/dot_config/exact_private_fish/exact_completions/hk.fish
+++ home/dot_config/exact_private_fish/exact_completions/hk.fish
@@ -1,1 +1,5 @@
-hk completion fish | source
+if set -l paths (path filter -fr $__fish_vendor_completionsdirs/hk.fish)
+    source $paths[1]
+else
+    __fish_cache_sourced_completions hk completion fish
+end
