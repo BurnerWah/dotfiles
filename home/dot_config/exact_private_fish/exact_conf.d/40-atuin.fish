7fe8c0fe548691e318ff17d4e55753911bda82ac
Burner <10004812+BurnerWah@users.noreply.github.com>
2026-02-02 11:07:43 -0600

    Made atuin not start up in WSL when it's misbehaving

--- home/dot_config/exact_private_fish/exact_conf.d/40-atuin.fish
+++ home/dot_config/exact_private_fish/exact_conf.d/40-atuin.fish
@@ -1,5 +1,9 @@
 if status is-interactive
     and command -q atuin
     and [ "$__CFBundleIdentifier" != "com.fournova.Tower3" ] # Tower just wants to see some env vars
+    # Bail if WSL is acting up so the terminal isn't flooded with errors
+    if [ -n "$WSL_INTEROP" ]
+        [ -d "$XDG_RUNTIME_DIR" ] || exit 0
+    end
     atuin init --disable-up-arrow fish | source
 end
