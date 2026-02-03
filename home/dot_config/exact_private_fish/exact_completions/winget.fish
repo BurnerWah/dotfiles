91337b6361fbe7009f1878fd0c54b5336007a928
Burner <10004812+BurnerWah@users.noreply.github.com>
2026-02-03 16:37:37 -0600

    Switched wsl winget completions to use carapace, and improved correctness of carapace.exe completion script

--- home/dot_config/exact_private_fish/exact_completions/winget.fish
+++ home/dot_config/exact_private_fish/exact_completions/winget.fish
@@ -1,5 +1,1 @@
-function _winget_completion
-    winget.exe complete --word (commandline -pct) --commandline (commandline -p) --position (commandline -pc | string length) | string trim -r
-end
-
-complete -c winget -x -a '(_winget_completion)'
+complete -c winget -w winget.exe
