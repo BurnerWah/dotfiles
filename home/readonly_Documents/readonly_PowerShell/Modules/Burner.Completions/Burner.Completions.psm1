2c830d8db389b7a01d52f00c9bf3181452de3ced
Burner <10004812+BurnerWah@users.noreply.github.com>
2026-02-03 19:00:05 -0600

    Switched windows to use json-based lazycomplete setup

--- home/readonly_Documents/readonly_PowerShell/Modules/Burner.Completions/Burner.Completions.psm1
+++ home/readonly_Documents/readonly_PowerShell/Modules/Burner.Completions/Burner.Completions.psm1
@@ -1,2 +1,1 @@
 . (Join-Path $PSScriptRoot "Scripts" "custom.ps1")
-. (Join-Path $PSScriptRoot "Scripts" "lazy-managed.ps1")
--- home/readonly_Documents/readonly_PowerShell/Modules/Burner.Completions/exact_Scripts/custom.ps1
+++ home/readonly_Documents/readonly_PowerShell/Modules/Burner.Completions/exact_Scripts/custom.ps1
@@ -1,12 +1,3 @@
-$Script:Winget = {
-    param($wordToComplete, $commandAst, $cursorPosition)
-    [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
-    $Local:word = $wordToComplete.Replace('"', '""')
-    $Local:ast = $commandAst.ToString().Replace('"', '""')
-    winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
-        [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-    }
-}
 $Script:Dotnet = {
     param($wordToComplete, $commandAst, $cursorPosition)
     dotnet complete --position $cursorPosition $commandAst.ToString() | ForEach-Object {
@@ -14,5 +5,4 @@ $Script:Dotnet = {
     }
 }
 
-Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock $Script:Winget
 Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock $Script:Dotnet
