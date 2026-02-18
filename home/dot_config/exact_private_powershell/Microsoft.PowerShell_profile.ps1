4d26cbc189b1e9013e9755dd2a6eb26f0446c52a
Burner <10004812+BurnerWah@users.noreply.github.com>
2026-02-18 14:44:49 -0600

    Tweaked unix pwsh profile to be more consistent with windows pwsh profile

--- home/dot_config/exact_private_powershell/Microsoft.PowerShell_profile.ps1
+++ home/dot_config/exact_private_powershell/Microsoft.PowerShell_profile.ps1
@@ -1,18 +1,21 @@
 $Env:PSModulePath = $Env:PSModulePath + (($IsWindows) ? ";" : ":") + (Join-Path $PSScriptRoot "Modules")
 if (($Host.Name -eq 'ConsoleHost') `
         -and ([System.Environment]::UserInteractive) `
-        -and -not ([System.Environment]::GetCommandLineArgs().Contains('-NonInteractive'))) {
+        -and -not ([System.Environment]::GetCommandLineArgs().Contains('-NonInteractive')) `
+        -and -not ([System.Environment]::GetCommandLineArgs().Contains('-Command'))) {
+    # Should be an interactive session
+
     if (Get-Command oh-my-posh -ErrorAction SilentlyContinue) {
-        oh-my-posh init pwsh | Invoke-Expression
+        oh-my-posh init pwsh --config jandedobbeleer | Invoke-Expression
     }
+
+    Import-Module -Name Burner.LazyComplete
+    Import-Module -Name Burner.Zoxide
+    Import-Module -Name Burner.TheFuck
 
     if (Get-Command carapace -ErrorAction SilentlyContinue) {
         Set-PSReadLineOption -Colors @{ "Selection" = "`e[7m" }
         Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
         carapace _carapace powershell | Out-String | Invoke-Expression
     }
-
-    Import-Module -Name Burner.LazyComplete
-    Import-Module -Name Burner.Zoxide
-    Import-Module -Name Burner.TheFuck
 }
