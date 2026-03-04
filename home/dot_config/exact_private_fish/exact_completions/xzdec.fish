ffbc63bafd5666c3439c714165cdde4d8548793d
Burner <10004812+BurnerWah@users.noreply.github.com>
2026-03-04 12:20:39 -0600

    even more completion updates

--- home/dot_config/exact_private_fish/exact_completions/xzdec.fish
+++
@@ -1,4 +0,0 @@
-complete -c xzdec -xa (_CM_suffixes .xz .txz)
-complete -c xzdec -s h -l help -d "Display help & exit"
-complete -c xzdec -s V -l version -d "Display version & exit"
-complete -c xzdec -s q -l quiet -d "Use twice to suppress errors"
