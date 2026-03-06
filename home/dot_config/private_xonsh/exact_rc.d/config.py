be6582f57d622afc28f1d18472d768576188322d
Burner <10004812+BurnerWah@users.noreply.github.com>
2026-03-06 13:14:59 -0600

    Updated ty config and tweaked xonsh config to pass ty checks

--- home/dot_config/private_xonsh/exact_rc.d/config.py
+++ home/dot_config/private_xonsh/exact_rc.d/config.py
@@ -4,10 +4,11 @@ from xonsh.built_ins import XSH
 
 env = XSH.env
 
-env["XONSH_CACHE_EVERYTHING"] = True
-env["AUTO_CD"] = True
-env["ENABLE_ASYNC_PROMPT"] = True
-env["XONSH_AUTOPAIR"] = True
+if env:
+    env["XONSH_CACHE_EVERYTHING"] = True
+    env["AUTO_CD"] = True
+    env["ENABLE_ASYNC_PROMPT"] = True
+    env["XONSH_AUTOPAIR"] = True
 
 aliases = XSH.aliases
 
